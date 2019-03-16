afc0cf8085f866fc8f6f16c44fc0cdb76e098258
N3R1UM <46677992+N3R1UM@users.noreply.github.com>
2019-03-16 23:43:36 +0800

    Create Main InfoRadar.py
    Main Python File To Start Program

---
+++ InfoRadar.py
@@ -0,0 +1,31 @@
+#coding=utf-8
+#Time : 2019-01-21
+__auther__ = 'n00b\'*'
+__version__ = '1.0.0'
+
+from radarlib.publicfunc import *
+
+import os
+import re
+import sys
+import time
+import socket
+import requests
+import subprocess
+
+
+
+# Main function
+# Created by Nerium at 2019/01/21
+# Modify by Nerium at 2019/03/04
+if __name__ == '__main__' :
+
+    # get domain by split terminal command
+    param = sys.argv[1:]
+    param_domain = param[-1]
+
+    # get Header info and return response to recompile all domain
+    res = Public_HeaderInfo(param_domain)
+
+    # get all second domain and create thread pool to scan them all
+    Public_SecDoamainIn(param_domain, res)
