f70ae0615ee3c406cb24ba4d49558b7166bca5f8
N3R1UM <46677992+N3R1UM@users.noreply.github.com>
2019-08-28 21:56:30 +0800

    Update Main；更新主程序
    Add thread to scan second domains, pack main class；增加线程池用以子域名一对一、封装主流程类

--- InfoRadar.py
+++ InfoRadar.py
@@ -1,17 +1,15 @@
 #coding=utf-8
 #Time : 2019-01-21
-__auther__ = 'n00b\'*'
-__version__ = '1.0.0'
+__author__ = 'dynamic program security team'
+__version__ = '1.0.2'
 
 from radarlib.publicfunc import *
+from radarlib.mainclass import *
 
 import os
-import re
 import sys
-import time
 import socket
-import requests
-import subprocess
+import threading
 
 
 
@@ -24,8 +22,19 @@ if __name__ == '__main__' :
     param = sys.argv[1:]
     param_domain = param[-1]
 
+    mainc = Mainclass()
     # get Header info and return response to recompile all domain
-    res = Public_HeaderInfo(param_domain)
+    res = Public_Response(param_domain)
+
+    mid_header = Public_HeaderInfo(param_domain)
+    mainc.domainip = mid_header[0]
+    mainc.server = mid_header[1]
+    mainc.cdnflag = mid_header[2]
 
     # get all second domain and create thread pool to scan them all
-    Public_SecDomainIn(param_domain, res)
+    mainc.secdomain = Public_SecDomainIn(param_domain, res, False)
+
+    #print(mainc.secdomain)
+    for i in range(len(mainc.secdomain)) :
+        t = sathread(mainc.secdomain[i], i)
+        t.start()
